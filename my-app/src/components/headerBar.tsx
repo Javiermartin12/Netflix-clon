a4ceee106e8f3cfa96bb1032f04d2cd30aa702d0
javier martin ruiz <javiermartinruiz@MacBook-Pro.local>
2024-05-14 13:31:46 +0200

    added some icon

--- my-app/src/components/headerBar.tsx
+++ my-app/src/components/headerBar.tsx
@@ -1,11 +1,26 @@
 import style from "../styles/componentsStyles/headerBar.module.css"
-
+import logo from "../assets/images/logo.jpg"
+import Image from 'next/image';
+import lupa from "../assets/images/lupablanca.png"
 
 
 export default function HeaderBar(){
 
 
     return(
-        <div className={style.headerContainer}></div>
+        <>
+        <div className={style.headerContainer}>
+            <Image src={logo} className={style.logo} alt="Logo moviehub" />
+            <div className={style.navLinks}>
+                <p>Home</p>
+                <p>Series</p>
+                <p>Movies</p>
+            </div>
+            <div className={style.endNavLinks}>
+            <Image src={lupa} alt="Lupa" width={24} height={24} className={style.lupaBlanca} />
+            </div>
+         </div>
+</>
+
     )
 }   
--- my-app/src/pages/homepage.tsx
+++ my-app/src/pages/homepage.tsx
@@ -6,7 +6,7 @@ import HeaderBar from "@/components/headerBar";
 
     return (
         <>
-        <div>HOME </div>
+        
         <HeaderBar />
         </>
     )
