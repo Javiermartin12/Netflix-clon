a485245462f74c1b888da9d4596d49095f834397
javier martin ruiz <javiermartinruiz@MacBook-Pro.local>
2024-05-24 13:37:39 +0200

    ad new changes

--- my-app/src/services/movieRequest.ts
+++ my-app/src/services/movieRequest.ts
@@ -1,43 +1,43 @@
-import axios from "axios";
+// import axios from "axios";
 
-const url = ""
+// const url = ""
 
-export const GetMovie = async ()=> {
-    try {
-        const response = await axios.get(url)
-        return response.data
-    } catch (error) {
-        console.log(error)
-        throw new Error("error")
-    }
-}
+// export const GetMovie = async ()=> {
+//     try {
+//         const response = await axios.get(url)
+//         return response.data
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("error")
+//     }
+// }
 
-export const CreateMovie = async ()=> {
-    try {
-        const response = await axios.post(url)
-        return response.data
-    } catch (error) {
-        console.log(error)
-        throw new Error("error")
-    }
-}
+// export const CreateMovie = async ()=> {
+//     try {
+//         const response = await axios.post(url)
+//         return response.data
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("error")
+//     }
+// }
 
-export const UpdateMovie = async() => {
-    try {
-        const response = await axios.patch(url)
-        return response.data
-    } catch (error) {
-        console.log(error)
-        throw new Error("error")
-    }
-}
+// export const UpdateMovie = async() => {
+//     try {
+//         const response = await axios.patch(url)
+//         return response.data
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("error")
+//     }
+// }
 
-export const DeleteMovie = async() => {
-    try {
-        const response = await axios.delete(url)
-        return response.data
-    } catch (error) {
-        console.log(error)
-        throw new Error("error")
-    }
-}
+// export const DeleteMovie = async() => {
+//     try {
+//         const response = await axios.delete(url)
+//         return response.data
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("error")
+//     }
+// }
